7633d89a975d2e8d11030ea993ed3c62e97e8026
Naturdragon <63045626+Naturdragon@users.noreply.github.com>
2023-12-12 22:11:42 +0100

    Created Project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.theos {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.theos to javafx.fxml;
+    exports com.example.theos;
+}
