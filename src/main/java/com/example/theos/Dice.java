a674a190da0ca56eab5f5609dfdcc87a92ef0291
Naturdragon <63045626+Naturdragon@users.noreply.github.com>
2023-12-19 20:45:06 +0100

    Added Die Class

---
+++ src/main/java/com/example/theos/Dice.java
@@ -0,0 +1,59 @@
+package com.example.theos;
+
+import java.util.Random;
+
+public class Dice {
+    // Random numbers use static to use the same random object continuously
+    static Random rnd = new Random();
+    private int charge;
+    private int[] dice;
+    private dieType type;
+
+    // Constructor for a normal die
+    public Dice()
+    {
+        charge = 0;
+        dice = new int[]{1, 2, 3, 4, 5, 6};
+        type = dieType.NormalDie;
+    }
+
+    // Constructor for a special die
+    public Dice(int[] specDie)
+    {
+        this.charge = 3;
+        this.dice = specDie;
+        type = dieType.SpecialDie;
+    }
+
+    public int getCharge()
+    {
+        return charge;
+    }
+
+    // Method for increasing charge by 1
+    public void addCharge()
+    {
+        charge++;
+    }
+
+    public dieType getDieType()
+    {
+        return type;
+    }
+
+    // Method for rolling a die
+    public int rollDie()
+    {
+        if (type == dieType.SpecialDie) { // In case of a special die, decrease the charge by 1
+            charge--;
+        }
+
+        return dice[rnd.nextInt(dice.length - 1)];
+    }
+
+    // Types of die
+    public enum dieType {
+        NormalDie,
+        SpecialDie
+    }
+}
