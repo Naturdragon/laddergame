7d069ee65bed2326c4a8a4f6bdb8747ddbbe0db0
mateobg <mateo.barrera.garcia@stud.fh-campuswien.ac.at>
2023-12-21 16:22:14 +0100

    Added Field class, added static member variables to Application class, adder playerTurn method to Application class (not programmed yet)

--- src/main/java/Graph/Graph.java
+++ src/main/java/Graph/Graph.java
@@ -41,7 +41,7 @@ public class Graph {
     }
 
     /*
-    Creates a Edge between two Vertexes
+    Creates an Edge between two Vertexes
     The Data of the two Vertexes to be connected
      */
     public <T> void addEdge(T Data1, T Data2)
