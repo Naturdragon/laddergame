b5c553d8ef2b1777746dc71119a514dc922b518e
Naturdragon <63045626+Naturdragon@users.noreply.github.com>
2024-03-14 14:15:47 +0100

    Fixed small Bug

--- src/main/java/com/example/theos/GameBoard.java
+++ src/main/java/com/example/theos/GameBoard.java
@@ -892,7 +892,7 @@ public class GameBoard {
         idleArrowTwo.play();
 
 
-        rootLayout.getChildren().addAll(pathOneArrow, pathTwoArrow);
+        mainLayout.getChildren().addAll(pathOneArrow, pathTwoArrow);
 
 
         // on mouse press the target arrow gets highlighted and on release both are removed form the layout and the rest of the move is done via crossingManager()
