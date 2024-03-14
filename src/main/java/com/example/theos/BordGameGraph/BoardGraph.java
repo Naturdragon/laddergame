b50524306bd0bf6e6ad1c17c8c8180899416f288
Aleynakurt27 <146325463+Aleynakurt27@users.noreply.github.com>
2024-03-14 22:31:35 +0100

    aleynas task again pleasee

--- src/main/java/com/example/theos/BordGameGraph/BoardGraph.java
+++ src/main/java/com/example/theos/BordGameGraph/BoardGraph.java
@@ -326,7 +326,7 @@ public class BoardGraph {
                         sequenTransis.getChildren().add(ladderPathTransition);
 
                         currentPlayer.setCurrentField(newVertexData);
-                        //new code Aleynas task to do hello:
+                        //new code Aleynas task to do hell:
                        // return sequenTransis;
                         if (newVertexData.getType() == Field.fieldType.LadderField) {
                             Field downVertexData = getNextVertex(newVertexData, forwardGraph, edgeType.LadderEdge);
